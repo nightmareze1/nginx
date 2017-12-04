9ac53fde7ecf50c18b29166c1815c0d89d3ec33a
Ezequiel Arielli <earielli@artear.com>
2017-12-04 17:05:35 -0300

    test circle deployment

--- kube.sh
+++ kube.sh
@@ -6,4 +6,4 @@ echo $REPO:$CIRCLE_TAG
 
 sed -i "s/<VERSION>/$(echo $CIRCLE_TAG | cut -c 1-7)/" template/deployment.yml
 sed -i "s/<REPO>/$(echo $REPO | cut -c 1-7)/" template/deployment.yml
-
+sed -i "s/<PROJECT>/$(echo $CIRCLE_PROJECT_REPONAME | cut -c 1-7)/" template/deployment.yml
