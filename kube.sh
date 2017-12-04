f310344a79277457466f34f8ea65ca864c80cadd
Ezequiel Arielli <earielli@artear.com>
2017-12-04 16:26:29 -0300

    test 2

--- kube.sh
+++ kube.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 set -e
-declare repo="$1" sha="$CIRCLE_SHA1" branch="$CIRCLE_BRANCH" tag="$CIRCLE_TAG"
-declare repo="$1" tag="$CIRCLE_TAG"
 
-kubectl apply -f template/deployment.yml
-kubectl apply -f template/svc.yml
-kubectl apply -f template/ing.yml
+echo $KUBE/$REPO:$CIRCLE_TAG
