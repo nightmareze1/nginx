e55c89beaf977a184700d8c9a55577fd5c37bef2
Ezequiel Arielli <earielli@artear.com>
2017-12-04 16:48:44 -0300

    TEST circle CI

--- kube.sh
+++ kube.sh
@@ -4,4 +4,6 @@ set -e
 
 echo $REPO:$CIRCLE_TAG
 
-sed -i "s/<VERSION>/$REPO:$CIRCLE_TAG/" template/deployment.yml
+sed -i "s/<VERSION>/$(echo $CIRCLE_TAG | cut -c 1-7)/" template/deployment.yml
+sed -i "s/<REPO>/$(echo $REPO | cut -c 1-7)/" template/deployment.yml
+
