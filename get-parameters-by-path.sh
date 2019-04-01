ed062da3091308946ad62824dbb810f9d918197c
zz <ezequiel@miroculus.com>
2019-04-01 13:56:38 -0300

    build variables

---
+++ get-parameters-by-path.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+PARAMETERS=`aws ssm get-parameters-by-path --path ${1} --with-decryption`
+
+#echo $PARAMETERS
+#echo ${PARAMETERS} | jq -c '.Parameters' | jq -c '.[]'
+
+for row in $(echo ${PARAMETERS} | jq -c '.Parameters' | jq -c '.[]'); do
+    KEY=$(basename $(echo ${row} | jq -c '.Name'))
+    VALUE=$(echo ${row} | jq -c '.Value')
+
+    KEY=`echo ${KEY} | tr -d '"'`
+    VALUE=`echo ${VALUE} | tr -d '"'`
+
+    export ${KEY}=${VALUE}
+    echo --build-arg ${KEY}="$"'{'${KEY}'}'  | tr '\n' ' ' >> vars.env
+   
+done
+
+# cat vars.env
+cat vars.env | awk '{ print "docker build --build-arg BUILD_NUMBER=${BUILD_NUMBER} " $0 " -t $REPOSITORY_URI:latest ."}' |bash && rm -rf vars.env
